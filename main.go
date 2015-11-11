358821abf3ca04af38d8bf9dd6fc3eda24ded0e4
Rex Lee <duguying2008@gmail.com>
2015-11-12 00:53:02 +0800

    router test

--- main.go
+++ main.go
@@ -1,9 +1,20 @@
 package main
 
 import (
+	"fmt"
 	"github.com/go-elaeagnus/elaeagnus"
 )
 
+func f1() {
+	fmt.Println("hello f1")
+}
+
+func f2() {
+	fmt.Println("hello f2")
+}
+
 func main() {
+	elaeagnus.Router("/hello1", f1)
+	elaeagnus.Router("/hello2", f2)
 	elaeagnus.Run()
 }
