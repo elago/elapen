1e33fe4bfeb5671d8c936df4416e8130e0f59794
Rex Lee <duguying2008@gmail.com>
2015-11-13 00:25:39 +0800

    action function with RequestContext param

--- main.go
+++ main.go
@@ -1,16 +1,15 @@
 package main
 
 import (
-	"fmt"
 	"github.com/go-elaeagnus/elaeagnus"
 )
 
-func f1() {
-	fmt.Println("hello f1")
+func f1(ctx elaeagnus.RequestContext) {
+	ctx.Write("hello world")
 }
 
-func f2() {
-	fmt.Println("hello f2")
+func f2(ctx elaeagnus.RequestContext) {
+	ctx.Write("hello function 2")
 }
 
 func main() {
