ee853d03da103755e46a7eb17bb78437e613ca6d
Rex Lee(李俊) <duguying2008@gmail.com>
2015-12-02 23:10:44 +0800

    reflect new and initialize struct

--- controller/controller.go
+++ controller/controller.go
@@ -2,12 +2,15 @@ package controller
 
 import (
 	"github.com/elago/ela"
+	"github.com/elago/orm"
 )
 
 func F1(ctx ela.RequestContext) {
+	orm.PrintModels()
 	ctx.Write("hello world")
 }
 
 func F2(ctx ela.RequestContext) {
+	orm.TestQuery()
 	ctx.Write("hello function 2")
 }
