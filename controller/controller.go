31576f3769813a07c10340c7e74320a959a31353
Rex Lee(李俊) <duguying2008@gmail.com>
2016-04-11 12:58:57 +0800

    test for error controller

--- controller/controller.go
+++ controller/controller.go
@@ -4,6 +4,10 @@ import (
 	"github.com/elago/ela"
 )
 
+func IndexCtrl(ctx ela.Context) {
+	ctx.ServeTemplate("index.html")
+}
+
 func F1(ctx ela.Context) {
 	ctx.Data["name"] = "lijun"
 	ctx.Data["id"] = -1
@@ -23,6 +27,12 @@ func F4(ctx ela.Context) {
 	ctx.ServeTemplate("test.html")
 }
 
-func Error500(ctx ela.Context) {
-	ctx.ServeError(500, "500.html")
+func Error500(ctx ela.Context, err error) {
+	ctx.SetHeader("Content-Type", "text/html")
+	ctx.Write("服务器内部错误，以下是错误内容\n"+err.Error())
+	// ctx.ServeError(500, "500.html")
+}
+
+func Error404(ctx ela.Context, err error) {
+	ctx.ServeError(404, "404.html")
 }
--- router/router.go
+++ router/router.go
@@ -6,9 +6,11 @@ import (
 )
 
 func init() {
+	ela.Router("/", controller.IndexCtrl)
 	ela.Router("/hello1", controller.F1)
 	ela.Router("/hello2", controller.F2)
 	ela.Router("/panic", controller.F3)
 	ela.Router("/hello3", controller.F4)
 	ela.Router("@500", controller.Error500)
+	ela.Router("@404", controller.Error404)
 }
