de427358dd3b3c3cec3b605d1aba23dd080b503e
Rex Lee(李俊) <duguying2008@gmail.com>
2016-04-19 17:19:41 +0800

    support mysql mode and sqlite mode

--- initial/db.go
+++ initial/db.go
@@ -1,7 +1,9 @@
 package initial
 
 import (
+	"fmt"
 	"github.com/elago/elapen/global"
+	_ "github.com/go-sql-driver/mysql"
 	"github.com/go-xorm/xorm"
 	"github.com/gogather/com/log"
 	_ "github.com/mattn/go-sqlite3"
@@ -10,11 +12,42 @@ import (
 var engine *xorm.Engine
 
 func initDbEngine() {
+	config := global.Config
+	engineType := config.GetStringDefault("_", "engine", "sqlite")
+
 	var err error
-	engine, err = xorm.NewEngine("sqlite3", "./data.sqlite")
-	if err != nil {
-		log.Redln(err)
+
+	if engineType == "mysql" {
+		username := config.GetStringDefault("mysql", "username", "root")
+		password := config.GetStringDefault("mysql", "password", "")
+		host := config.GetStringDefault("mysql", "host", "127.0.0.1")
+		port := config.GetIntDefault("mysql", "port", 3306)
+		dbName := "elapen"
+		dsn := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?charset=utf8", username, password, host, port, dbName)
+
+		engine, err = xorm.NewEngine("mysql", dsn)
+		if err != nil {
+			log.Redln(err)
+		} else {
+			log.Greenf("use mysql ")
+			log.Bluef("%s\n", dsn)
+		}
+	} else {
+		dsn := config.GetStringDefault("sqlite", "path", "./data.sqlite")
+
+		engine, err = xorm.NewEngine("sqlite3", dsn)
+		if err != nil {
+			log.Redln(err)
+		} else {
+			log.Greenf("use mysql ")
+			log.Bluef("%s\n", dsn)
+		}
 	}
 
-	global.Engine = engine
+	runMode := config.GetBoolDefault("_", "runmode", false)
+	if runMode {
+		global.Engine = engine
+	}
+
+	global.Engine.ShowSQL(true)
 }
